210904ba7775d035b8554aed3093f37700885c2d
igorviana98 <41646402+igorviana98@users.noreply.github.com>
2025-11-13 16:34:13 -0300

    Reorganize SQL script for database creation

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -1,23 +1,21 @@
--- Criação do banco (opcional)
+
 CREATE DATABASE covid19_dw;
 
--- Conectar ao banco antes de executar os comandos abaixo
 
--- Tabela de Estados
 CREATE TABLE dim_estado (
     id_estado  PRIMARY KEY,
     nome_estado VARCHAR(50) NOT NULL,
     sigla CHAR(2) NOT NULL UNIQUE
 );
 
--- Tabela de Tempo (Mês/Ano)
+
 CREATE TABLE dim_tempo (
     id_tempo  PRIMARY KEY,
     mes VARCHAR(10) NOT NULL,
     ano INT NOT NULL
 );
 
--- Tabela Fato Covid-19
+
 CREATE TABLE fato_covid (
     id_fato  PRIMARY KEY,
     id_estado INT NOT NULL REFERENCES dim_estado(id_estado),
@@ -28,4 +26,5 @@ CREATE TABLE fato_covid (
     obitos_acumulados INT NOT NULL,
     novos_vacinados INT NOT NULL,
     vacinados_acumulados INT NOT NULL
+
 );
